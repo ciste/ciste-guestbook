eb3afe9dc4fded2c966c708f9148f0bb4bec62d5
Daniel E. Renfer <duck@kronkltd.net>
2012-01-04 17:47:37 -0500

    ring dep

--- project.clj
+++ project.clj
@@ -2,5 +2,6 @@
   :description "Guestbook example for Ciste"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [ciste "0.2.0-SNAPSHOT"]
+                 [ring "1.0.0"]
                  ]
   :main ciste-guestbook.core)
