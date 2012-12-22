be8a02c420998703a5d2378aa590c359fa0ecab8
Daniel E. Renfer <duck@kronkltd.net>
2012-12-22 12:34:43 -0500

    bump dependencies

--- project.clj
+++ project.clj
@@ -3,11 +3,11 @@
   :description "Guestbook example for Ciste"
   :author "Daniel E. Renfer <duck@kronkltd.net>"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [ciste "0.3.0-SNAPSHOT"]
-                 [ring "1.0.0"]
-                 [log4j "1.2.16"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [ciste "0.4.0-SNAPSHOT"]
+                 [ring/ring-core "1.1.6"]
+                 [log4j "1.2.17"]
                  ]
   :main ciste.runner
-  :plugins [[codox "0.6.1"]
-            [lein-midje "2.0.0-SNAPSHOT"]])
+  :plugins [[codox "0.6.4"]
+            [lein-midje "2.0.3"]])
